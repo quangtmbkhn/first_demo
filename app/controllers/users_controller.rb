d722c3fd5d9e9a1a77b8a48d54ef646772965ece
tmquang1999 <quang99991111@gmail.com>
2018-06-27 20:39:49 +0700

    first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
-def show
-	@user = User.find(params[:id])
-  @microposts = @user.microposts.paginate(page: params[:page], per_page: 5)
-end
+  def show
+  	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 5)
+  end
 
-def new
-	@user = User.new
-end
-def create
+  def new
+  	@user = User.new
+  end
+  def create
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
